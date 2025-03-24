cba8f6ce0753052af44ca4c42736871275f23177
Nikita <example@mail>
2025-03-24 21:09:19 +0300

    29 after corr

--- catalog/services.py
+++ catalog/services.py
@@ -15,13 +15,13 @@ def get_products_from_cache():
     return products
 
 
-def get_products_by_category_from_cache():
+def get_products_by_category_from_cache(category_id):
     if not CACHE_ENABLED:
-        return Product.objects.filter(category=Category.objects.get(pk=id))
-    key = f"products_by_category_{id}"
+        return Product.objects.filter(category=Category.objects.get(pk=category_id))
+    key = f"products_by_category_{category_id}"
     products = cache.get(key)
     if products is not None:
         return products
-    products = Product.objects.filter(category=Category.objects.get(pk=id))
+    products = Product.objects.filter(category=Category.objects.get(pk=category_id))
     cache.set(key, products)
     return products
